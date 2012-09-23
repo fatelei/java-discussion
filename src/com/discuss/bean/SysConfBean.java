d8b88b90d42113213e6a372fac1af121dd80b8e0
Ocean <weew@wew.com>
2012-09-23 20:50:33 +0800

    x

--- src/com/discuss/bean/SysConfBean.java
+++ src/com/discuss/bean/SysConfBean.java
@@ -1,6 +1,6 @@
 package com.discuss.bean;
 
 public class SysConfBean {
-	public  static final int  UserListPageNum = 1;				//user	list	page
-	public  static final int  DisListPageNum = 1;				//dis	list	page
+	public  static final int  UserListPageNum = 2;				//user	list	page
+	public  static final int  DisListPageNum = 2;				//dis	list	page
 }
